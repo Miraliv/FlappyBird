61cf4b88b14ca7f0bb112daeb2c8918d6e6585de
Kauã Vilarim <kauavilarim@gmail.com>
2024-11-14 23:38:17 -0300

    add Score system and reset game option

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -65,6 +65,8 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     Timer gameLoop;
     Timer placePipesTimer;
     boolean gameOver = false;
+    double score = 0;
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.blue);
@@ -124,6 +126,15 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
             Pipe pipe = pipes.get(i);
             g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
         }
+
+        //score
+        g.setColor(Color.white);
+        g.setFont(new Font("Arial", Font.PLAIN, 32));
+        if(gameOver){
+            g.drawString("Game Over: " + String.valueOf((int)score), 10, 35);
+        }else{
+            g.drawString(String.valueOf((int) score), 10, 35);
+        }
     }
 
 
@@ -136,6 +147,11 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
         for(int i=0; i<pipes.size(); i++){
             Pipe pipe = pipes.get(i);
             pipe.x += velocityX;
+
+            if(!pipe.passed && bird.x > pipe.x + pipe.width){
+                pipe.passed = true;
+                score += 0.5;
+            }
 
             if(collision(bird, pipe)){
                 gameOver = true;
@@ -165,6 +181,16 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener {
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_SPACE){
             velocityY = -9;
+            if(gameOver){
+                //restart the game
+                bird.y = birdY;
+                velocityY = 0;
+                pipes.clear();
+                score = 0;
+                gameOver = false;
+                gameLoop.start();
+                placePipesTimer.start();
+            }
         }
     }
     @Override
